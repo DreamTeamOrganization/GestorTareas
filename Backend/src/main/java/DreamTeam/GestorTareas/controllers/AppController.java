341540a0e65ba37e831b1b72746b6ec21f38535c
Alejandro <alejoreyes124@gmail.com>
2025-04-04 11:35:11 -0500

    First controller

---
+++ Backend/src/main/java/DreamTeam/GestorTareas/controllers/AppController.java
@@ -0,0 +1,16 @@
+package DreamTeam.GestorTareas.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins="https://localhost:5173")
+public class AppController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hola desde el backend";
+    }
+}
